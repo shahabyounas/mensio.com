ecd3d5409e61fb8b34a1d2d62afe336787a77d48
Shahab Younas <shahab@saal.ai>
2021-04-16 20:16:27 +0400

    logs

--- api/db.js
+++ api/db.js
@@ -5,6 +5,9 @@ const PASSWORD = encodeURIComponent('123Hamza!@#');
 
 const uri = `mongodb+srv://${DB_USER}:${PASSWORD}@cluster0.skgo0.mongodb.net/mensio?retryWrites=true&w=majority`
 
+console.log(process.env.MONGODB_URI)
+console.log(uri)
+
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
